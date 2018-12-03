1a0b0c02067bae7bf1f0f1cb0e97e6f46bf07468
yh-Gabriel <214524224@qq.com>
2018-12-03 17:53:48 +0800

    抓取歌手数据

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,15 +1,40 @@
 <template>
-    <div>
-      歌手页面
-    </div>
+  <div class="singer">
+
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "singer"
+  import {getSingerList} from "../../api/singer";
+  import {ERR_OK} from "../../api/config";
+
+  export default {
+    name: "singer",
+    data() {
+      return {
+        singers: []
+      }
+    },
+    created() {
+      this._getSingerList();
+    },
+    methods:{
+      _getSingerList(){
+        getSingerList().then((res)=>{
+          if(res.code===ERR_OK){
+            this.singers=res.data.list;
+            console.log(res.data.list)
+          }
+        })
+      }
     }
+  }
 </script>
 
-<style scoped>
-
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .singer
+    position: fixed
+    top: 88px
+    bottom: 0
+    width: 100%
 </style>
