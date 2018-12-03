7121c0718957ebe946549615f4885e6491c0294a
yh-Gabriel <214524224@qq.com>
2018-12-03 23:23:48 +0800

    歌手详细页面子路由配置以及转场动画实现

--- src/router/index.js
+++ src/router/index.js
@@ -4,14 +4,15 @@ import Recommend from '@/components/recommend/recommend'
 import Singer from '@/components/singer/singer'
 import Rank from '@/components/rank/rank'
 import Search from '@/components/search/search'
+import SingerDetail from '@/components/singer-detail/singer-detail'
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path:'/',
-      redirect:'/recommend'
+      path: '/',
+      redirect: '/recommend'
     },
     {
       path: '/recommend',
@@ -21,16 +22,23 @@ export default new Router({
     {
       path: '/singer',
       name: 'Singer',
-      component: Singer
+      component: Singer,
+      children: [
+        {
+          path:':id',
+          name:'SingerDetail',
+          component:SingerDetail
+        }
+      ]
     },
     {
       path: '/rank',
-      name:'Rank',
+      name: 'Rank',
       component: Rank
     },
     {
       path: '/search',
-      name:'Search',
+      name: 'Search',
       component: Search
     }
   ]
