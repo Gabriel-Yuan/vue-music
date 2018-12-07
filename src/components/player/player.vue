f844609fdd1a35dc5b58b050b555ec999f5fc42d
yh-Gabriel <214524224@qq.com>
2018-12-07 18:34:19 +0800

    代码完成

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -70,7 +70,7 @@
               <i @click="next" class="icon-next"></i>
             </div>
             <div class="icon i-right">
-              <i class="icon icon-not-favorite"></i>
+              <i class="icon" @click="toggleFavorite(currentSong)" :class="getFavoriteIcon(currentSong)"></i>
             </div>
           </div>
         </div>
@@ -98,7 +98,7 @@
     <playlist ref="playlist"></playlist>
     <audio ref="audio"
            :src="currentSong.url"
-           @canplay="ready"
+           @play="ready"
            @error="error"
            @timeupdate="updateTime"
            @ended="end"
@@ -283,6 +283,9 @@
       },
       getLyric() {
         this.currentSong.getLyric().then((lyric) => {
+          if(this.currentSong.lyric!==lyric){
+            return;
+          }
           this.currentLyric = new Lyric(lyric, this.handleLyric);
           if (this.playing) {
             this.currentLyric.play();
@@ -422,7 +425,8 @@
         if (this.currentLyric) {
           this.currentLyric.stop();
         }
-        setTimeout(() => {
+        clearTimeout(this.timer);
+        this.timer=setTimeout(() => {
           this.$refs.audio.play();
           this.getLyric();
         },1000)
